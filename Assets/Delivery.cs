176bd71030f1058cbbe6e8163b6e5f11fec567c1
dylongaddi <93099493+dylongaddi@users.noreply.github.com>
2023-07-13 00:47:43 -0700

    added camera following car, basic level layout, package pick up and customer delivery interactions

---
+++ Assets/Delivery.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Ow");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Package")
+        {
+            Debug.Log("Package picked up");
+        }
+
+        if (collision.tag == "Customer")
+        {
+            Debug.Log("Package delivered to customer");
+        }
+
+    }
+}
